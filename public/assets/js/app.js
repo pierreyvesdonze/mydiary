936af773ca5abd46158f1ebcbee1ac5e3a80cb8d
py eye <pierreyvesdonze.pro@gmail.com>
2023-09-29 12:31:30 +0200

    init

---
+++ public/assets/js/app.js
@@ -0,0 +1,13 @@
+var app = {
+    
+    init: () => {
+
+        /**
+        * *****************************
+        * L I S T E N E R S
+        * *****************************
+        */
+    },
+}
+
+document.addEventListener('DOMContentLoaded', app.init)
---
+++ src/Form/RegistrationFormType.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
+
+class RegistrationFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('email', TextType::class, [
+                'required' => true
+            ])
+            ->add('plainPassword', RepeatedType::class, [
+                'type' => PasswordType::class,
+                'invalid_message' => 'Les 2 mots de passe doivent correspondre',
+                'mapped' => false,
+                'required' => true,
+                'first_options'  => ['label' => 'Mot de passe'],
+                'second_options' => ['label' => 'Confirmation du mot de passe'],
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Entrez un mot de passe',
+                    ]),
+                    new Length([
+                        'min' => 6,
+                        'minMessage' => 'Votre mot de passe doit faire au minimum {{ limit }} characters',
+                        // max length allowed by Symfony for security reasons
+                        'max' => 4096,
+                    ]),
+                ],
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "M'enregistrer",
+                'attr' => [
+                    'class' => "btn custom-btn"
+                ]
+            ]);
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
