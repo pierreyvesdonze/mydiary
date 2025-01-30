c09ea61cf315894545e8c4a38dcb83323984f0f5
pierreyvesdonze <pierreyvesdonze.pro@gmail.com>
2025-01-30 15:48:40 +0100

    remove ckeditor

--- config/bundles.php
+++ config/bundles.php
@@ -14,6 +14,5 @@ return [
     SymfonyCasts\Bundle\ResetPassword\SymfonyCastsResetPasswordBundle::class => ['all' => true],
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
-    FOS\CKEditorBundle\FOSCKEditorBundle::class => ['all' => true],
     Knp\Bundle\PaginatorBundle\KnpPaginatorBundle::class => ['all' => true],
 ];
